1a983be9dc45f60e1f63fee8abd7f839e5a86ed4
naveen2314 <naveen2314@yahoo.com>
2024-05-25 18:56:41 +0400

    update code for module

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-module "secrets_manager" {
-  source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.1.2"
 
-  create                  = var.create
-  name                    = var.secret_name
-  description             = var.description
-  tags                    = var.tags
-  recovery_window_in_days = var.recovery_window_in_days
+################################################################################
+# Secret
+################################################################################
 
+resource "aws_secretsmanager_secret" "this" {
+  count = var.create ? 1 : 0
+
+  description                    = var.description
+  force_overwrite_replica_secret = var.force_overwrite_replica_secret
+  kms_key_id                     = var.kms_key_id
+  name                           = var.name
+  name_prefix                    = var.name_prefix
+  recovery_window_in_days        = var.recovery_window_in_days
+
+  tags = var.tags
 }
--- output.tf
+++ output.tf
@@ -1,9 +1,18 @@
+################################################################################
+# Secret
+################################################################################
+
 output "secret_arn" {
-  description = "The ARN of the created secret"
-  value       = module.secrets_manager.secret_arn
+  description = "The ARN of the secret"
+  value       = try(aws_secretsmanager_secret.this[0].arn, null)
 }
 
 output "secret_id" {
-  description = "The name of the created secret"
-  value       = module.secrets_manager.secret_id
+  description = "The ID of the secret"
+  value       = try(aws_secretsmanager_secret.this[0].id, null)
+}
+
+output "secret_replica" {
+  description = "Attributes of the replica created"
+  value       = try(aws_secretsmanager_secret.this[0].replica, null)
 }
